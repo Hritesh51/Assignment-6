c4162acf391327c11d766bf11039e7f0bf0292d4
Hritesh Bhardwaj <61631866+Hritesh51@users.noreply.github.com>
2021-04-18 23:24:57 +0530

    Add files via upload

---
+++ BlogApp-main/BlogApp-main/README.md
@@ -0,0 +1,7 @@
+# BlogApp
+Coding Blocks Assignment 6
+User Name:- Nishit Password:- Nishit
+User Name:- Aryan Password:- Aryan
+User Name:- Sabel sir Password:- Sabel sir
+for login
+http://localhost:3000/login
---
+++ BlogApp-main/BlogApp-main/server.js
@@ -0,0 +1,128 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb+srv://NishitSood:Nishit316@cluster0.9zlkd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+  })    
+  .then(() => {
+    console.log("Database Connected");
+   })
+   .catch(err => {
+    console.log("DB Not Connected");
+    console.log(err);
+  })
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+/*  EXPRESS SETUP  */
+
+app.use(express.static(__dirname));
+
+const bodyParser = require('body-parser');
+const expressSession = require('express-session')({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('App listening on port ' + port));
+
+/*  PASSPORT SETUP  */
+
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* MONGOOSE SETUP */
+
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const Schema = mongoose.Schema;
+const UserDetail = new Schema({
+  username: String,
+  password: String
+});
+
+UserDetail.plugin(passportLocalMongoose);
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+
+/* PASSPORT LOCAL AUTHENTICATION */
+
+passport.use(UserDetails.createStrategy());
+
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
+
+/* ROUTES */
+
+const connectEnsureLogin = require('connect-ensure-login');
+
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local',
+  (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) { 
+      return res.redirect('/login?info=' + info); 
+    }
+
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+
+      return res.redirect('/');
+    });
+
+  })(req, res, next);
+});
+
+app.get('/login',
+  (req, res) => res.sendFile('html/login.html',
+  { root: __dirname })
+);
+
+app.get('/',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.sendFile('html/index.html', {root: __dirname})
+);
+
+app.get('/private',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.sendFile('html/private.html', {root: __dirname})
+);
+
+app.get('/user',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.send({user: req.user})
+);
+
+/* REGISTER SOME USERS */
+
+UserDetails.register({username:'Nishit', active: false}, 'Nishit');
+UserDetails.register({username:'Aryan', active: false}, 'Aryan');
+UserDetails.register({username:'Sabel sir', active: false}, 'Sabel sir');
+
+/*passport ka liya*/
+
+/*app.listen(5000)*/
